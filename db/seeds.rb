379e81d4752a367bf285414c559470f46d760dd5
Philippe Francisco <lpfrancisco99@gmail.com>
2022-11-22 13:10:01 +0100

    New seed and some views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+User.create!(first_name: "Maxence", last_name: "Labre", email: "test@gmail.com", password: "1212jJK12")
+10.times do
 Exercise.create!(name: "Deliveroo", description: "Exo MVC", price_per_day: 50, category: "A", response_time: 50, user_id: 1)
-Exercise.create!(name: "UberEats", description: "Exo Manger", price_per_day: 30, category: "B", response_time: 20, user_id: 2)
+Exercise.create!(name: "UberEats", description: "Exo Manger", price_per_day: 30, category: "B", response_time: 20, user_id: 1)
+end
