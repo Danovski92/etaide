89093e9dd0505af3fdcd95240b096a8696bd7c11
Philippe Francisco <lpfrancisco99@gmail.com>
2022-11-23 00:44:17 +0100

    Implementer création de booking dans la show de exercises, test seed et cards sur dashboard, plus liens navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-# post = Post.new(
-#   title: Faker::Company.name,
-#   url: Faker::Address.zip,
-#   votes: rand(0..5)
-# )
-# post.save!
-
 puts "Creating new database................................."
 puts "......................................................"
 x = 0
-user = User.new(
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  email: Faker::Internet.email,
-  password: "123456BA"
-)
-user.save!
-puts "....Adding ...#{x + 1}..Users to DB.............."
+5.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "123456BA"
+  )
+  user.save!
+  puts "....Adding ...#{x + 1}..Users to DB.............."
+end
 
-50.times do
-
+25.times do
   x += 1
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "123456BA"
+  )
+  user.save!
+  puts "....Adding ...#{x}..Users to DB.............."
 
-    user = User.new(
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      email: Faker::Internet.email,
-      password: "123456BA"
-    )
-    user.save!
-    puts "....Adding ...#{x}..Users to DB.............."
-
-    5.times do
+  5.times do
     exercise = Exercise.new(
       name: Faker::Space.galaxy,
       description: Faker::BossaNova.song,
@@ -48,12 +34,11 @@ puts "....Adding ...#{x + 1}..Users to DB.............."
     )
     exercise.save!
     puts "....Adding ...#{x * 5}..Exercises to DB.............."
-
-
     day = rand(1..30).to_s
     month = rand(1..12).to_s
-
-    5.times do
+    num = rand(1..4).to_i
+    case num
+    when 1
       booking = Booking.new(
         start_date: "#{day}/01/2022",
         end_date: "#{day}/#{month}/2022",
@@ -61,41 +46,31 @@ puts "....Adding ...#{x + 1}..Users to DB.............."
         exercise_id: exercise.id
       )
       booking.save!
-      puts "....Adding ...#{x * 5 * 5}..Bookings to DB.............."
+    when 2
+      booking = Booking.new(
+        start_date: "#{day}/01/2022",
+        end_date: "#{day}/#{month}/2022",
+        user_id: user.id - 2,                                 #### ne peut pas etre = à l'user_id de l'exercise
+        exercise_id: exercise.id
+      )
+      booking.save!
+    when 3
+      booking = Booking.new(
+        start_date: "#{day}/01/2022",
+        end_date: "#{day}/#{month}/2022",
+        user_id: user.id - 3,                                 #### ne peut pas etre = à l'user_id de l'exercise
+        exercise_id: exercise.id
+      )
+      booking.save!
+    when 4
+      booking = Booking.new(
+        start_date: "#{day}/01/2022",
+        end_date: "#{day}/#{month}/2022",
+        user_id: user.id - 4,                                 #### ne peut pas etre = à l'user_id de l'exercise
+        exercise_id: exercise.id
+      )
+      booking.save!
+      puts "....Adding ...#{x * 5}..Bookings to DB.............."
     end
   end
 end
-
-
-
-
-
-  # exo3 = Exercise.new(name: Faker::Space.galaxy, description: Faker::BossaNova.song, price_per_day: rand(0..500), category: "C", response_time: 20)
-  # p exo3.user = user3
-  # exo3.save
-
-  # exo4 = Exercise.new(name: Faker::Space.galaxy, description: Faker::BossaNova.song, price_per_day: rand(0..500), category: "D", response_time: 20)
-  # exo4.user = user4
-  # exo4.save
-
-  # exo5 = Exercise.new(name: Faker::Space.galaxy, description: Faker::BossaNova.song, price_per_day: rand(0..500), category: "E", response_time: 20)
-  # exo5.user = user5
-  # exo5.save
-
-
-  # book2 = Booking.new(start_date: "02/03/2022", end_date: "03/02/2022", user_id: 2, exercise_id: 2)
-  # book2.save
-
-  # book3 = Booking.new(start_date: "03/03/2022", end_date: "04/02/2022", user_id: 3, exercise_id: 3)
-  # book3.save
-
-  # book4 = Booking.new(start_date: "04/04/2022", end_date: "05/02/2022", user_id: 4, exercise_id: 4)
-  # book4.save
-
-  # book5 = Booking.new(start_date: "05/05/2022", end_date: "06/02/2022", user_id: 5, exercise_id: 5)
-  # book5.save
-
-
-
-
-# pour les Booking > user_id != exercise_id
