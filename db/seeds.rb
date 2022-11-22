19d84c0bfe2dbbd0e33d031705c214579f19e17e
Philippe Francisco <lpfrancisco99@gmail.com>
2022-11-22 15:59:17 +0100

    New seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,56 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create!(first_name: "Maxence", last_name: "Labre", email: "test@gmail.com", password: "1212jJK12")
-10.times do
-Exercise.create!(name: "Deliveroo", description: "Exo MVC", price_per_day: 50, category: "A", response_time: 50, user_id: 1)
-Exercise.create!(name: "UberEats", description: "Exo Manger", price_per_day: 30, category: "B", response_time: 20, user_id: 1)
-end
+
+puts "Creating new database..."
+
+Booking.destroy_all
+Exercise.destroy_all
+User.destroy_all
+
+
+user1 = User.create!(first_name: "Maxence", last_name: "Hestia", email: "test@gmail.com", password: "1213jJK12")
+user2 = User.create!(first_name: "Philippe", last_name: "Hestia", email: "test1@gmail.com", password: "1212jJK12")
+user3 = User.create!(first_name: "Daniel", last_name: "Hestia", email: "test2@gmail.com", password: "1214jJK12")
+user4 = User.create!(first_name: "Nicolas", last_name: "Hestia", email: "test3@gmail.com", password: "1215jJK12")
+user5 = User.create!(first_name: "Bertrand", last_name: "Hestia", email: "test4@gmail.com", password: "1216jJK12")
+
+puts "..."
+
+exo1 = Exercise.new(name: "Deliveroo", description: "Exo MVC", price_per_day: 50, category: "A", response_time: 50)
+exo1.user = user1
+exo1.save
+
+exo2 = Exercise.new(name: "UberEats", description: "Exo Manger", price_per_day: 30, category: "B", response_time: 20)
+exo2.user = user2
+exo2.save
+
+exo3 = Exercise.new(name: "UberEats", description: "Exo Manger", price_per_day: 30, category: "B", response_time: 20)
+exo3.user = user3
+exo3.save
+
+exo4 = Exercise.new(name: "UberEats", description: "Exo Manger", price_per_day: 30, category: "B", response_time: 20)
+exo4.user = user4
+exo4.save
+
+exo5 = Exercise.new(name: "UberEats", description: "Exo Manger", price_per_day: 30, category: "B", response_time: 20)
+exo5.user = user5
+exo5.save
+
+
+book1 = Booking.new(start_date: "01/01/2022", end_date: "02/02/2022", user_id: 1, exercise_id: 1)
+book1.save
+
+book2 = Booking.new(start_date: "02/02/2022", end_date: "02/02/2022", user_id: 2, exercise_id: 2)
+book2.save
+
+book3 = Booking.new(start_date: "03/03/2022", end_date: "02/02/2022", user_id: 3, exercise_id: 3)
+book3.save
+
+book4 = Booking.new(start_date: "04/04/2022", end_date: "02/02/2022", user_id: 4, exercise_id: 4)
+book4.save
+
+book5 = Booking.new(start_date: "05/05/2022", end_date: "02/02/2022", user_id: 5, exercise_id: 5)
+book5.save
+
+puts "Added 5 Exercises, 5 Bookings and 5 User to database !"
