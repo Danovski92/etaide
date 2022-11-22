7ce4050864fcc6e6410f8c9fb06fc1c240fc6c2d
Philippe Francisco <lpfrancisco99@gmail.com>
2022-11-22 18:35:57 +0100

    New seed, you have to db:drop/create/migrate/seed, 50 exs/bookings and 5 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,52 +12,77 @@
 # )
 # post.save!
 
-puts "Creating new database..."
-
-user1 = User.create!(first_name: "Maxence", last_name: "Hestia", email: "test@gmail.com", password: "1213jJK12")
-user2 = User.create!(first_name: "Philippe", last_name: "Hestia", email: "test1@gmail.com", password: "1212jJK12")
-user3 = User.create!(first_name: "Daniel", last_name: "Hestia", email: "test2@gmail.com", password: "1214jJK12")
-user4 = User.create!(first_name: "Nicolas", last_name: "Hestia", email: "test3@gmail.com", password: "1215jJK12")
-user5 = User.create!(first_name: "Bertrand", last_name: "Hestia", email: "test4@gmail.com", password: "1216jJK12")
-
-puts "..."
-
+puts "Creating new database................................."
+puts "......................................................"
+x=0
 10.times do
 
-  exo1 = Exercise.new(name: Faker::Space.galaxy, description: Faker::BossaNova.song, price_per_day: rand(0..500), category: "A", response_time: 50)
-  exo1.user = user1
-  exo1.save
+  x += 1
+  user = User.new(
+    first_name: "Philippe",
+    last_name: "Francisco",
+    email: "salut@gmail#{x}.com",
+    password: "123456BA"
+  )
+  user.save!
 
-  exo2 = Exercise.new(name: Faker::Space.galaxy, description: Faker::BossaNova.song, price_per_day: rand(0..500), category: "B", response_time: 20)
-  exo2.user = user2
-  exo2.save
+  exercise = Exercise.new(
+    name: Faker::Space.galaxy,
+    description: Faker::BossaNova.song,
+    price_per_day: rand(0..500),
+    category: "A",
+    response_time: 50,
+    user_id: user.id
+  )
+  exercise.save!
 
-  exo3 = Exercise.new(name: Faker::Space.galaxy, description: Faker::BossaNova.song, price_per_day: rand(0..500), category: "C", response_time: 20)
-  exo3.user = user3
-  exo3.save
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Blockchain::Bitcoin.address
+  )
+  user.save!
 
-  exo4 = Exercise.new(name: Faker::Space.galaxy, description: Faker::BossaNova.song, price_per_day: rand(0..500), category: "D", response_time: 20)
-  exo4.user = user4
-  exo4.save
-
-  exo5 = Exercise.new(name: Faker::Space.galaxy, description: Faker::BossaNova.song, price_per_day: rand(0..500), category: "E", response_time: 20)
-  exo5.user = user5
-  exo5.save
-
-  book1 = Booking.new(start_date: "01/01/2022", end_date: "02/02/2022", user_id: 1, exercise_id: 1)
-  book1.save
-
-  book2 = Booking.new(start_date: "02/03/2022", end_date: "03/02/2022", user_id: 2, exercise_id: 2)
-  book2.save
-
-  book3 = Booking.new(start_date: "03/03/2022", end_date: "04/02/2022", user_id: 3, exercise_id: 3)
-  book3.save
-
-  book4 = Booking.new(start_date: "04/04/2022", end_date: "05/02/2022", user_id: 4, exercise_id: 4)
-  book4.save
-
-  book5 = Booking.new(start_date: "05/05/2022", end_date: "06/02/2022", user_id: 5, exercise_id: 5)
-  book5.save
-
-  puts "Added 5 Exercises, 5 Bookings and 5 User to database !"
+  p user.id
+  booking = Booking.new(
+    start_date: "01/01/2022",
+    end_date: "02/02/2022",
+    user_id: user.id,                                 #### ne peut pas etre = à l'user_id de l'exercise
+    exercise_id: exercise.id
+  )
+  booking.save!
+  puts "Added 1 Exercises, 1 Bookings and 1 User to database !"
 end
+
+puts "......................................................"
+
+  # exo3 = Exercise.new(name: Faker::Space.galaxy, description: Faker::BossaNova.song, price_per_day: rand(0..500), category: "C", response_time: 20)
+  # p exo3.user = user3
+  # exo3.save
+
+  # exo4 = Exercise.new(name: Faker::Space.galaxy, description: Faker::BossaNova.song, price_per_day: rand(0..500), category: "D", response_time: 20)
+  # exo4.user = user4
+  # exo4.save
+
+  # exo5 = Exercise.new(name: Faker::Space.galaxy, description: Faker::BossaNova.song, price_per_day: rand(0..500), category: "E", response_time: 20)
+  # exo5.user = user5
+  # exo5.save
+
+
+  # book2 = Booking.new(start_date: "02/03/2022", end_date: "03/02/2022", user_id: 2, exercise_id: 2)
+  # book2.save
+
+  # book3 = Booking.new(start_date: "03/03/2022", end_date: "04/02/2022", user_id: 3, exercise_id: 3)
+  # book3.save
+
+  # book4 = Booking.new(start_date: "04/04/2022", end_date: "05/02/2022", user_id: 4, exercise_id: 4)
+  # book4.save
+
+  # book5 = Booking.new(start_date: "05/05/2022", end_date: "06/02/2022", user_id: 5, exercise_id: 5)
+  # book5.save
+
+
+
+
+# pour les Booking > user_id != exercise_id
