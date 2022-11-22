ed94578a2cb3cceb957d5caa46ed54047801a121
Nicolas Grumberg <nicolas@vroomvroom.fr>
2022-11-22 11:56:14 +0100

    exercises controller + home view

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,14 +1,28 @@
 class ExercisesController < ApplicationController
 
   def index
+    @exercises = Exercise.all
   end
 
   def show
+    @exercise = Exercise.find(params[:id])
+
   end
 
   def new
+    @exercise = Exercise.new
   end
 
   def create
+    @exercice = Exercise.new(exercise_params)
+    @exercise.save
+    redirect_to dashboard_path #to be changed later?
   end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :description, :price_per_day, :category, :response_time)
+  end
+
 end
