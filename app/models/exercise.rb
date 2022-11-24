90d68d87bc1673482e9bd716a510038a66ca7875
Nicolas Grumberg <nicolas@vroomvroom.fr>
2022-11-24 16:18:40 +0100

    add images to users and exercises

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,7 +2,7 @@ class Exercise < ApplicationRecord
   CATEGORIES = ["Front-end", "Back-end", "Base de données", "Transverse", "Initiation"]
   has_many :bookings
   belongs_to :user
-  validates :name, :description, :response_time, :price_per_day, :category, presence: true
+  validates :name, :description, :response_time, :price_per_day, :category, :image, presence: true
   validates :price_per_day, :response_time, numericality: true
   validates :category, inclusion: { in: CATEGORIES }
 
